2734067beb731ef3b43b2058e17f4a04e1c48fbf
MohammadAli ghanavati <polamgh@users.noreply.github.com>
2026-06-03 01:55:17 -0400

    Initial Commit

---
+++ AzadiTunnel/AzadiTunnelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzadiTunnelApp.swift
+//  AzadiTunnel
+//
+//  Created by Ali Ghanavati on 2026-06-03.
+//
+
+import SwiftUI
+
+@main
+struct AzadiTunnelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AzadiTunnel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AzadiTunnel
+//
+//  Created by Ali Ghanavati on 2026-06-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
